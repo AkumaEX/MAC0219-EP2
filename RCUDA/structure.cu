e82a172bfc66a6c0120e78e2e24e1408be4bea34
Julio Kenji Ueda <julio.ueda@usp.br>
2018-06-04 17:04:28 -0300

    Redutor com CUDA, Sequencial e Gerador de Matrizes adicionado

---
+++ RCUDA/structure.cu
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "structure.h"
+
+int *load_structure(char *filepath, int *num_matrices) {
+    int i, j, id;
+    FILE *file = fopen(filepath, "r");
+
+    if (file == NULL) {
+        printf("Problema ao abrir o arquivo. Verifique o caminho do arquivo, saindo...\n");
+        exit(1);
+    }
+
+    if (!fscanf(file, "%d", num_matrices)) {
+        printf("Problema ao ler o arquivo, saindo...\n");
+        exit(1);
+    }
+
+    int *structure = (int *) malloc(9 * *num_matrices * sizeof(int*));
+
+    for (j = 0; j < *num_matrices; j++) {
+        for (i = 0, id = j; i < 9; i++, id += *num_matrices){
+            if (!fscanf(file, "%d", &structure[id])) {
+                printf("Problema ao ler o arquivo, saindo...\n");
+                exit(1);
+            }
+        }
+    }
+
+    fclose(file);
+    return structure;
+}
+
+void print_structure(int *structure, int num_matrices) {
+    int i;
+    for (i = 0; i < 9 * num_matrices; i++) {
+        if (i != 0 && i % num_matrices == 0)
+            printf("\n");
+        printf("%d\t", structure[i]);
+    }
+    printf("\n");
+}
+
+void print_matrix(int *matrix) {
+    int i;
+    for (i = 0; i < 9; i++) {
+        if (i != 0 && i % 3 == 0)
+            printf("\n");
+        printf("%d\t", matrix[i]);
+    }
+    printf("\n");
+}
---
+++ RCUDA/structure.h
@@ -0,0 +1,10 @@
+#ifndef EP2_STRUCTURE_H
+#define EP2_STRUCTURE_H
+
+int *load_structure(char *filepath, int *num_matrices);
+
+void print_structure(int *structure, int num_matrices);
+
+void print_matrix(int *matrix);
+
+#endif //EP2_STRUCTURE_H
